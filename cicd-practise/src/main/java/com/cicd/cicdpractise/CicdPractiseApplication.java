8105cce4e94b0844c76718bb3f83236f45746371
Pavan Ambi <pavaambi@in.ibm.com>
2021-01-15 20:44:48 -0500

    initialize application

---
+++ cicd-practise/src/main/java/com/cicd/cicdpractise/CicdPractiseApplication.java
@@ -0,0 +1,14 @@
+package com.cicd.cicdpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPractiseApplication {
+
+	public static void main(String[] args) {
+		System.out.println("starting main application--->");
+		SpringApplication.run(CicdPractiseApplication.class, args);
+	}
+
+}
